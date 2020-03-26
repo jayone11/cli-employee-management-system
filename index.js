309798f04845d1b0e85f3ba0199bbc6e6adf8231
Jeff B <neustellar@gmail.com>
2020-03-26 14:34:55 -0700

    required dependencies in index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const cliArt = require("figlet");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const chalk = require("chalk");
+const util = require("util");
+const mysql = require("mysql");
